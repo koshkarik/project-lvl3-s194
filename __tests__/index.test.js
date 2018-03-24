b53faea0663d1d39637e79cab9ab09086b4d461b
Koshkarik <koshkarevj@mail.ru>
2018-03-24 17:44:04 +0300

    improve test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,6 +10,9 @@ import saveData from '../src';
 const host = 'https://fake.com';
 const imagePath = '/images/somefile.jpg';
 const cssPath = '/styles/style.css';
+const folderForAssets = '/fake-com_files';
+const downloadedImgName = 'images-somefile.jpg';
+const downloadedCssName = 'styles-style.css';
 
 const fixturesPath = '__tests__/__fixtures__';
 axios.defaults.adapter = httpAdapter;
@@ -17,14 +20,23 @@ axios.defaults.adapter = httpAdapter;
 test('download page and save it', async () => {
   const nockPage = await fsMz.readFile(path.join(fixturesPath, 'testPage.html'), 'utf8');
   const expectedPage = await fsMz.readFile(path.join(fixturesPath, 'expectedTestPage.html'), 'utf8');
-  const testImage = await fsMz.readFile(path.join(fixturesPath, imagePath));
+  const testImage = await fsMz.readFile(path.join(fixturesPath, imagePath), 'utf8');
   const testCssFile = await fsMz.readFile(path.join(fixturesPath, cssPath), 'utf8');
+
   nock(host).get('/').reply(200, nockPage);
   nock(host).get(imagePath).reply(200, testImage);
   nock(host).get(cssPath).reply(200, testCssFile);
+
   const tmpFolder = fs.mkdtempSync(path.join(os.tmpdir(), 'bar-'));
   await saveData(tmpFolder, host);
   const pathToFile = path.join(tmpFolder, 'fake-com.html');
+  const pathToImageFile = path.join(tmpFolder, folderForAssets, downloadedImgName);
+  const pathToCssFile = path.join(tmpFolder, folderForAssets, downloadedCssName);
   const downloadedPage = await fsMz.readFileSync(pathToFile, 'utf8');
+  const downloadedImage = await fsMz.readFileSync(pathToImageFile, 'utf8');
+  const downloadedCss = await fsMz.readFileSync(pathToCssFile, 'utf8');
+
   expect(downloadedPage).toBe(expectedPage);
+  expect(downloadedImage).toBe(testImage);
+  expect(downloadedCss).toBe(testCssFile);
 });
